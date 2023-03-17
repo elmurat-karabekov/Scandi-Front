32db781a23fa81cf1265ab7a616996e0a4eec91e
elmurat-karabekov <elmurat.karabekov@scandiweb.com>
2023-03-17 23:32:09 +0600

    final version

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Checkout/view/frontend/web/js/sidebar': {
+            'Magento_Checkout/js/sidebar': {
                 'Magento_Checkout/js/sidebar-mixin': true
             }
         }
